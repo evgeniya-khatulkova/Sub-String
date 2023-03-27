e7ca456d429988443ed78801ab6bbc8f29a0f18e
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-27 19:59:53 +0200

    resturn result as a hash listing

--- Sub-String.rb
+++ Sub-String.rb
@@ -1,5 +1,9 @@
+
 def substring(word, dictionary)
   result = []
   dictionary.each {|one| result.push(one) if word.include?(one)}
-  result
+  result.reduce(Hash.new(0)) do |key, value|
+  key[value] += 1
+  key
+  end
 end
